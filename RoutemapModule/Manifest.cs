e92d192b4c55519a1fcd0d1c0b4abad507d5a3cd
etcetraw <29414565+etcetraw@users.noreply.github.com>
2020-04-13 11:08:55 -0600

    Add files via upload

---
+++ RoutemapModule/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Route Map Module",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "1.0.0",
+    Description = "Test",
+    Category = "Transit"
+)]
---
+++ RoutemapModule/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.BackgroundTasks;
+using OrchardCore.Modules;
+using System;
+namespace RoutemapModule
+{
+  public  class Startup : StartupBase
+    {
+        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                    name: "RoutemapModule",
+                    areaName: "RoutemapModule",
+                    pattern: "RoutemapModule/Test/Index",
+                    defaults: new { controller = "Test", action = "Index" }
+                );
+        }
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            
+       
+
+        }
+    }
+}
